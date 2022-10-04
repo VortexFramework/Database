f3002c891e3284e98099b41677a42d49ba56c9c5
Connor Graham <thecg1997@gmail.com>
2022-10-05 00:17:09 +0100

    Package: Add required credentials for Github workflow to publish Gradle package

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,27 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.0"
+    `maven-publish`
 }
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/VortexFramework/Database")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
+}
+
 
 group = "com.vortexframework"
 version = "0.1"
